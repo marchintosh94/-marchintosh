9c9d9c9e3f8df06d107f79dc6a93f02164247aed
Marco Baratto <marcobaratto@icloud.com>
2022-10-12 00:14:50 +0200

    update pdf library

--- pages/api/resume-en.ts
+++ pages/api/resume-en.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { generatePdf } from "html-pdf-node-ts"
+import { generatePdf } from "html-pdf-node"
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
