c4ae9330c01d999ff30db76917790c3fc06a81d6
Marco Baratto <marcobaratto@icloud.com>
2022-10-12 00:19:47 +0200

    update pdf library integration

--- pages/api/resume-en.ts
+++ pages/api/resume-en.ts
@@ -1,11 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { generatePdf } from "html-pdf-node"
+const htmlPdf = require("html-pdf-node")
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
   const file = {url: `${process.env.FE_BASE_URL}/resume/en`}
 
-  generatePdf(file, {format: 'A4', printBackground: true, scale: 0.58}).then(pdfBuffer => {
+  htmlPdf.generatePdf(file, {format: 'A4', printBackground: true, scale: 0.58}).then((pdfBuffer: Buffer) => {
     res.setHeader('Content-Type', 'application/pdf')
     res.setHeader('Content-Disposition', 'attachment; filename=marcobaratto-resume.pdf')
     res.setHeader('Content-Length', pdfBuffer.length)
